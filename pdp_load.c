d5f4462db963f42efcd35572e2b43464794917db
Arman Urazgulov <Urazgulov.A@phystech.edu>
2020-04-26 23:14:05 +0300

    file fixed

--- pdp_load.c
+++ pdp_load.c
@@ -10,17 +10,10 @@ typedef word Adress;				//16bit
 
 byte mem[MEMSIZE];
 
-<<<<<<< HEAD
 byte b_read(Adress adr);
 void b_write(Adress adr, byte b);
 word w_read(Adress adr);
 void w_write(Adress adr, word w);
-=======
-byte b_read  (Adress adr);
-void b_write (Adress adr, byte b);
-word w_read  (Adress adr);
-void w_write (Adress adr, word w);
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
 //void load_file();
 void load_file(const char* filename);
 void mem_dump(Adress adr, word w);
@@ -34,11 +27,7 @@ void test_mem() {
     
     /*Adress a = 4;
     byte b1 = 0x0b;
-<<<<<<< HEAD
     byte b0 = 0x0a;
-=======
-    b0 = 0x0a;
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
     word w = 0x0b0a;
     b_write(a, b0);
     b_write(a + 1, b1);
@@ -52,7 +41,6 @@ void test_mem() {
     word wress = w_read(a);
     printf("%04hx = %04hx\n", w, wress);
     assert(w == wress);*/
-<<<<<<< HEAD
 
 	/*Adress a = 8;
 	word w = 0xff0c;
@@ -71,24 +59,17 @@ void test_mem() {
 	b_write(a + 1, b1);
 	word wress = w_read(a);
 	printf("ww/br \t %04hx = %02hhx%02hhx\n", wress, b1, b0);
-	printf("%06o = %03o%03o\n", wress, b1, b0);
+	printf("%06 = %03o%03o\n", wress, b1, b0);
 	assert(w == wress);
-=======
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
 }
 
 void test_load(const char* filename) {
 	load_file(filename);
-<<<<<<< HEAD
 	mem_dump(0x40, 30);
-=======
-	mem_dump(0x40, 4);
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
 }
 
 int main(int argc, char* argv[]) {
 	test_mem();
-<<<<<<< HEAD
 	if (argc > 1)
 		test_load(argv[argc - 1]);
 	else {
@@ -111,36 +92,17 @@ word w_read(Adress adr) {
 		perror("Adress should be even for read word");
 		exit(1);
 	}
-=======
-	test_load(argv[argc - 1]);
-	return 0;
-}
-
-byte b_read  (Adress adr) {
-	return mem[adr];
-}
-
-void b_write (Adress adr, byte b) {
-	mem[adr] = b;
-}
-
-word w_read  (Adress adr) {
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
 	word w = ((word)mem[adr + 1]) << 8;
 	w = w | mem[adr];
 	//printf("w = %04x\n", w);
 	return w;
 }
 
-<<<<<<< HEAD
 void w_write(Adress adr, word w) {
 	if (adr % 2) {
 		perror("Adress should be even for write word");
 		exit(1);
 	}
-=======
-void w_write (Adress adr, word w) {
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
 	word w1 = w >> 8;
 	byte b1 = ((byte)w);
 	mem[adr] = b1;
@@ -163,7 +125,6 @@ void w_write (Adress adr, word w) {
 
 void load_file(const char* filename) {
 	FILE* stream = fopen(filename, "r");
-<<<<<<< HEAD
 	if (stream == NULL) {
 		perror("Wrong file");
 		exit(1);
@@ -173,13 +134,6 @@ void load_file(const char* filename) {
 		fscanf(stream, "%x%x", &adr, &n);
 		for (int i = 0; i < n; ++i) {
 			unsigned int _byte;
-=======
-	while (!feof(stream)) {
-		int adr, n;
-		fscanf(stream, "%x%x", &adr, &n);
-		for (int i = 0; i < n; ++i) {
-			int _byte;
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
 			fscanf(stream, "%x", &_byte);
 			b_write(adr, _byte);
 			++adr;
@@ -195,9 +149,6 @@ void mem_dump(Adress start, word n) {
 		printf("%06o\n", w);
 		start += 2;
 	}
-<<<<<<< HEAD
 	if (n % 2)
 		mem_dump(start, 2);
-=======
->>>>>>> 3b12fb45d93561df7acb8ea0f5e39ee80217426e
 }
