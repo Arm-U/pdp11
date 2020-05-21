086c6b674abb457f56a889db149f0cd130acf6ee
Arman Urazgulov <Urazgulov.A@phystech.edu>
2020-05-21 21:12:37 +0300

    added get_mr and run func supplemented

--- pdp_run.c
+++ pdp_run.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 
 extern Command cmd[];
+extern word reg[8];
+
+Argument ss, dd;
+
+Argument get_mr(word w);
 
 void run() {
 	pc = 01000;
@@ -10,21 +15,60 @@ void run() {
 		word w = w_read(pc);
 		trace("%06o %06o: ", pc, w);
 		pc += 2;
-		if (w == 0) {
-			trace("halt \n");
-			do_halt();
+		for (int i = 0; i <= 3; ++i) {
+			if ( (w & cmd[i].mask) == cmd[i].opcode) {
+				trace(cmd[i].name);
+				trace(" ");
+				if (cmd[i].params == 0) {}
+				else if (cmd[i].params == 1) {
+					dd = get_mr(w);
+				}
+				else if (cmd[i].params == 2) {
+					ss = get_mr(w >> 6);
+					dd = get_mr(w);
+				}
+				cmd[i].do_func();
+				break;
+			}
+			if (i == 3) {
+					trace("unknown command");
+					do_nothing();
+					break;
+			}
 		}
-		else if ((w & 0170000) == 0010000) {
-			trace("mov \n");
-			do_mov();
-		}
-		else if ((w & 0170000) == 0060000) {
-			trace("add \n");
-			do_add();
-		}
-		else {
-			trace("unknown command\n");
-			do_nothing();
-		}
+		trace("\n");
 	}
 }
+
+Argument get_mr(word w) {
+	Argument res;
+	int r = w & 7; // register
+	int mode = (w >> 3) & 7;
+	switch(mode) {
+		case 0:    // R3
+			res.adr = r;
+			res.val = reg[r];
+			trace("R%o ", r);
+			break;
+		case 1:    // (R3)
+			res.adr = reg[r];
+			res.val = w_read(res.adr);   // to do b_read
+			trace("R%o ", r);
+			break;
+		case 2:    // (R3)+
+			res.adr = reg[r];
+			res.val = w_read(res.adr);   // to do b_read
+			reg[r] += 2;                 // to do +1
+			if (r == 7)
+				trace("#%o ", res.val);
+			else
+				trace("R%o ", r);
+			break;
+		default:
+			fprintf(stderr, 
+				"Mode %o NOT IMPLEMENTED YET!\n", mode);
+			exit(1);
+	}
+	return res;
+}
+ 
