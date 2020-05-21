972f38daff296461b65928acc3e346564ffec4fc
Arman Urazgulov <Urazgulov.A@phystech.edu>
2020-05-21 21:14:52 +0300

    added print_reg and do funcs supplemented

--- pdp.c
+++ pdp.c
@@ -2,18 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern Argument ss, dd;
+
+void print_reg() {
+	for (int i = 0; i < 8; ++i) {
+		printf("r%d: %06o ", i, reg[i]);
+	}
+	printf("\n");
+}
+
 void do_halt() {
-	printf("THE END1!!!\n");
+	trace("\n");
+	print_reg();
+	trace("THE END!!!\n");
 	exit(0);
 }
 
-void do_mov() {}
+void do_mov() {
+	w_write(dd.adr, ss.val);
+}
 
-void do_add() {}
+void do_add() {
+	w_write(dd.adr, dd.val + ss.val);
+}
 
 void do_nothing() {}
 
 Command cmd[] = {
-	{0170000, 0010000, "mov", do_mov},
-	{0170000, 0060000, "add", do_add}
+	{0170000, 0010000, "mov", do_mov, 2},
+	{0170000, 0060000, "add", do_add, 2},
+	{0xFFFF , 0000000, "halt", do_halt, 0}
 };
