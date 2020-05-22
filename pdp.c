6ce1d18ee65ea12e229e5c5b2762833b9961391f
Arman Urazgulov <Urazgulov.A@phystech.edu>
2020-05-22 14:54:15 +0300

    added clr and sob

--- pdp.c
+++ pdp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern Argument ss, dd;
+extern Argument ss, dd, nn, r;
 
 void print_reg() {
 	for (int i = 0; i < 8; ++i) {
@@ -30,8 +30,20 @@ void do_nothing() {
 	exit(1);
 }
 
+void do_sob() {
+	trace("#%06o", pc - 2 * nn.val);
+	if (--reg[r.adr] != 0)
+		pc -= 2 * nn.val;
+}
+
+void do_clear() {
+	w_write(dd.adr, 0);
+}
+
 Command cmd[] = {
 	{0170000, 0010000, "mov", do_mov, 3},
 	{0170000, 0060000, "add", do_add, 3},
-	{0xFFFF , 0000000, "halt", do_halt, 0}
+	{0xFFFF , 0000000, "halt", do_halt, 0},
+	{0177000, 0077000, "sob", do_sob, 12},
+	{0177700, 0005000, "clr", do_clear, 1}
 };
