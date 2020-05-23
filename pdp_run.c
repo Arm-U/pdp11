3cbdb302afe62c414602f923c14a72b9abeebf7a
Arman Urazgulov <Urazgulov.A@phystech.edu>
2020-05-23 19:56:02 +0300

    added mode 6 and get_r_last for find reg if reg in last 3 bytes

--- pdp_run.c
+++ pdp_run.c
@@ -11,6 +11,7 @@ Argument get_mr(word w);
 Argument get_nn(word w);
 Argument get_r(word w);
 Argument get_xx(word w);
+Argument get_r_last(word w);
 
 int is_byte;
 
@@ -40,6 +41,9 @@ void run() {
 				if (cmd[i].params & HAS_XX) {
 					xx = get_xx(w);
 				}
+				if (cmd[i].params & HAS_RL) {
+					r = get_r_last(w);
+				}
 				cmd[i].do_func();
 				//trace("\n");
 				//print_reg();
@@ -54,6 +58,7 @@ Argument get_mr(word w) {
 	Argument res;
 	int r = w & 7; // register
 	int mode = (w >> 3) & 7;
+	int x;
 	switch(mode) {
 		case 0:    // R3
 			res.adr = r;
@@ -62,7 +67,7 @@ Argument get_mr(word w) {
 			break;
 		case 1:    // (R3)
 			res.adr = reg[r];
-			if (is_byte)
+			if (is_byte && r != 6 && r !=7)
 				res.val = b_read(res.adr);
 			else 
 				res.val = w_read(res.adr);   // to do b_read
@@ -70,7 +75,7 @@ Argument get_mr(word w) {
 			break;
 		case 2:    // (R3)+
 			res.adr = reg[r];
-			if (is_byte) {
+			if (is_byte && r != 6 && r !=7) {
 				res.val = b_read(res.adr);  
 				reg[r] += 1;  
 			}
@@ -90,7 +95,7 @@ Argument get_mr(word w) {
 			reg[r] += 2;
 			break;
 		case 4:   //  -(R3)
-			if (is_byte) {
+			if (is_byte && r != 6 && r !=7) {
 				reg[r] -= 1;
 				res.adr = reg[r];
 				res.val = b_read(res.adr);
@@ -104,6 +109,13 @@ Argument get_mr(word w) {
 				trace("#%06o ", res.val);
 			else
 				trace("-(R%o) ", r);
+			break;
+		case 6:   // x(R3)
+			x = w_read(pc);
+			pc += 2;
+			res.adr = x + reg[r];
+			res.val = w_read(res.adr);
+			trace("%o(R%o) ", x, r);
 			break;
 		default:
 			fprintf(stderr, 
@@ -136,6 +148,14 @@ Argument get_xx(word w) {  // xx
 	else
 		val = (w & 0x00FF) - 0400;
 	res.val = val;
+	return res;
+}
+
+Argument get_r_last(word w) {  //reg
+	Argument res;
+	int r = w & 7; // register
+	res.adr = r;
+	trace("R%o ", r);
 	return res;
 }
  
